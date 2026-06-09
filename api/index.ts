b699affe54dc94d30e48ff01dada0e243664d667
amgeekz <79773348+amgeekz@users.noreply.github.com>
2026-06-09 17:31:19 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,6 @@
 import compiledServer from "../dist/server.js";
 
 // Support both direct export and default wrapper configurations
-const app = compiledServer.default || compiledServer;
+const app = (compiledServer as any).default || compiledServer;
 
 export default app;
-
