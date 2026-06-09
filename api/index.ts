341a63d43abbbb7067ee163264f1bd6d1048f9ed
amgeekz <79773348+amgeekz@users.noreply.github.com>
2026-06-09 17:24:50 +0700

    Add files via upload

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,8 @@
 // @ts-ignore
-import compiledServer from "../dist/server.cjs";
+import compiledServer from "../dist/server.js";
 
-// Mendukung direct-export maupun default-wrapper untuk kompatibilitas penuh
+// Support both direct export and default wrapper configurations
 const app = compiledServer.default || compiledServer;
 
 export default app;
+
