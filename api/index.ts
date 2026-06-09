17e313964d6934bfb25172b0546f44108937ec8d
amgeekz <79773348+amgeekz@users.noreply.github.com>
2026-06-09 17:20:44 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,7 @@
-import app from "../server";
+// @ts-ignore
+import compiledServer from "../dist/server.cjs";
+
+// Mendukung direct-export maupun default-wrapper untuk kompatibilitas penuh
+const app = compiledServer.default || compiledServer;
+
 export default app;
